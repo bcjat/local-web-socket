7e0527e09d005727c5369551152e976bb845cb11
Balvant Jat <bc.jat11@gmail.com>
2020-05-14 23:09:15 +0530

    setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,33 @@
 import { Component } from '@angular/core';
 import { WebSocketServer } from "@ionic-native/web-socket-server/ngx";
 
+//declare var wsserver:any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
-  constructor(private webserver:WebSocketServer ) {
-    this.webserver.start( 0, {
+    constructor(private webserver:WebSocketServer ) {
+  }
 
-    }).subscribe({
-      next:server=>{
-        console.log('success: ',server);
-      },
+  initServer(){
+    
+     this.webserver.start( 8888, {}).subscribe({
+      next: server => console.log(`Listening on ${server.addr}:${server.port}`),
       error:error=>{
-        console.log('error occured: ',error);
+        console.log('error occured: '+error);
       }
     })
-
+    
+    this.webserver.watchOpen().subscribe({
+      next: data=>{
+      console.log(data);
+      },
+      error: error => console.log(error)
+    })
+  
+   
   }
-  
-  
   
 }
